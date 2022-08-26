683cc41f9975330a712015bfd720d1b86b04260c
zhaoyang <zhaoyang@lyky.cn>
2022-08-26 15:05:29 +0800

    update

---
+++ js/common/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?1c4096e911d2bae34c8c0d3074b5b63a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
