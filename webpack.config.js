d451d848fd2f13bd02b615a93646e670f5e2331c
zhaoyang <zhaoyang@lyky.cn>
2024-09-24 09:05:24 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const nodeExternals = require('webpack-node-externals');
+// const nodeExternals = require('webpack-node-externals');
 const TerserPlugin = require("terser-webpack-plugin");
 // const BundleDeclarationsWebpackPlugin = require("bundle-declarations-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -25,39 +25,39 @@ const commonConfig = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
-    externals: nodeExternals(),
+    // externals: nodeExternals(),
 }
 
 module.exports = [
-    Object.assign({}, commonConfig, {
-        target: 'electron-main',
-        entry: {
-            "index": path.resolve(__dirname, 'src/index.ts')
-        },
-        output: {
-            path: path.resolve(__dirname, "./dist"),
-            library: "electronPosPrinter",
-            filename: "[name].js",
-            libraryTarget: "umd",
-            umdNamedDefine: true,
-        },
-        optimization: {
-            minimize: true,
-            minimizer: [new TerserPlugin()],
-        },
-        plugins: [
-            // new BundleDeclarationsWebpackPlugin(),
-        ],
+    // Object.assign({}, commonConfig, {
+    //     target: 'electron-main',
+    //     entry: {
+    //         "index": path.resolve(__dirname, 'src/index.ts')
+    //     },
+    //     output: {
+    //         path: path.resolve(__dirname, "./dist"),
+    //         library: "electronPosPrinter",
+    //         filename: "[name].js",
+    //         libraryTarget: "umd",
+    //         umdNamedDefine: true,
+    //     },
+    //     optimization: {
+    //         minimize: true,
+    //         minimizer: [new TerserPlugin()],
+    //     },
+    //     plugins: [
+    //         // new BundleDeclarationsWebpackPlugin(),
+    //     ],
 
-    }),
+    // }),
     Object.assign({}, commonConfig, {
         target: 'electron-renderer',
         entry:  {
-            "renderer": path.resolve(__dirname, 'src/renderer/renderer.ts')
+            "renderer": path.resolve(__dirname, 'pages')
         },
         output: {
-            path: path.resolve(__dirname, "./dist/renderer"),
-            filename: "[name].js"
+            path: path.resolve(__dirname, "./dist"),
+            filename: "[name]"
         },
         optimization: {
             minimize: true,
@@ -69,7 +69,7 @@ module.exports = [
         plugins: [
             new HtmlWebpackPlugin({
                 title: 'Print Previews-------',
-                template: 'src/renderer/index.html'
+                template: 'pages/picture/index.html'
             }),
             new MiniCssExtractPlugin({
                 filename:"[name].min.css"})
